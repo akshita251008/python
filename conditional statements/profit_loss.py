230cbdd7d2a484c4b50d7ee4a9bda6ce79a44670
akshita251008 <www.akshitaaaaaa.com@gmail.com>
2026-06-09 10:56:38 +0530

    profit loss

---
+++ conditional statements/profit_loss.py
@@ -0,0 +1,8 @@
+actual_cost = float(input("please enter the actual price: "))
+sale_amount = float(input("please enter the sales amount: "))
+
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("total profit = {} in rupees".format(amount))
+else:
+    print("no profit!!!")
