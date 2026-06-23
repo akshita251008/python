16f78bd3abcfc3a753d8ab221621c2e3de9f9d62
akshita251008 <www.akshitaaaaaa.com@gmail.com>
2026-06-23 14:16:33 +0530

    test

---
+++ test/test.py
@@ -0,0 +1,15 @@
+'''ld a game where the computer picks a secret number between 1 and 50. You have 5 attempts to guess it. After every wrong guess your program shows a hint telling you how close you are. Remaining lives are shown as hearts after each attempt.'''
+import random
+secret =random.secret(1,50)
+
+attempts = 5
+guess = int(input("guess the number between 1-50"))
+
+if guess == sectret:
+    ptint("conratulation you guessed")
+
+else:
+    print("")
+
+ 
+ 
